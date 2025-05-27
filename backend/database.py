a3fbe1e832e41483b8e543bd20e502d7102e2af6
peadarx1 <walshp27@tcd.ie>
2025-05-27 15:13:41 +0100

    Deleted false database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-db_url = "sqlite:///database.db"
+db_url = "sqlite:///backend/database.db"
 
 engine = create_engine(db_url, connect_args={"check_same_thread": False})
 
